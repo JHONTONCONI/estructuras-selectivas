94ade6abe22535231c8be75ce541d0121d9d6259
JHONTONCONI <btaedwin123@gmail.com>
2025-05-28 17:21:02 -0400

    MEJORE EL ARCHIVO

--- primer-ejercicio.cpp
+++ primer-ejercicio.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 int main (){
-		double c, d, cc;
-	cout<<nn ***************************;
-	cout<< INGRESE EL VALOR DE LA COMPRA;
-	cout<< **************************<<endl;
-	cin>>c;
-	if (c>1000){
-		d=c*20/100;
-		cc=c-d;
-		cout<<VALOR FINAL DE LA COMPRA DESPUES DEL DESCUENTO: <<cc<<endl;	
+		double compra, descuento, preciofinal;
+	cout<<"\n\n     ***************************";
+	cout<<"     INGRESE EL VALOR DE LA COMPRA";
+	cout<<"     **************************"<<endl;
+	cin>>compra;
+	if (compra>1000){
+		descuento=compra*20/100;
+		preciofinal=compra-descuento;
+		cout<<"VALOR FINAL DE LA COMPRA DESPUES DEL DESCUENTO: "<<preciofinal<<endl;	
 	}else{
-		cc=c;
-		cout<<VALOR FINAL DE LA COMPRA: <<cc<<endl;
+		preciofinal=compra;
+		cout<<"VALOR FINAL DE LA COMPRA:   "<<preciofinal<<endl;
     }
     return 0;
 	
