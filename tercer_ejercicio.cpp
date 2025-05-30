c4ed23a0ee3ac20f6ed89b72bf0c819cc7e8599d
JHONTONCONI <btaedwin123@gmail.com>
2025-05-30 09:18:45 -0400

    MEJORE LA INTERFAZ

--- tercer_ejercicio.cpp
+++ tercer_ejercicio.cpp
@@ -4,21 +4,27 @@ using namespace std;
 
 int main (){
 	double pago, cantidadcamisas, preciocamisa, pagofinal;
-	cout<<nn *****************************************<<endl;
-	cout<< INGRESE LA CANTIDAD DE CAMISAS QUE COMPRO<<endl;
-	cout<< *****************************************<<endl;
+	cout<<"\n\n     *****************************************"<<endl;
+	cout<<"     INGRESE LA CANTIDAD DE CAMISAS QUE COMPRO"<<endl;
+	cout<<"     *****************************************"<<endl;
 	cin>>cantidadcamisas;
-	cout<<nn *************************************************************************<<endl;
-	cout<< INGRESE EL PRECIO DE LA CAMISA TENIENDO EN CUENTA QUE CADA UNA VALE IGUAL<<endl;
-	cout<< *************************************************************************<<endl;
+	cout<<"\n\n     **************************************************************************"<<endl;
+	cout<<"     INGRESE EL PRECIO DE LA CAMISA TENIENDO EN CUENTA QUE CADA UNA VALE IGUAL"<<endl;
+	cout<<"     *************************************************************************"<<endl;
 	cin>>preciocamisa;
-	pago=cantidadcamisas*preciocamisa;
-	if(cantidadcamisas>3){
-		pagofinal=pago-(pago*20/100);	
+	
+	pago = cantidadcamisas * preciocamisa;
+	
+	if(cantidadcamisas > 3){
+		pagofinal = pago - (pago*20/100);	
 	}
 	else{
-		pagofinal=pago-(pago*10/100);
+		pagofinal = pago - (pago*10/100);
 	}
-	cout<<EL PAGO FINAL A REALIZAR ES: <<pagofinal<<endl;
+	cout<<"\n\n     ***************************"<<endl;
+	cout<<"     EL PAGO FINAL A REALIZAR ES:"<<endl;
+	cout<<"     ****************************"<<endl;
+	cout<<endl;
+	cout<<pagofinal<<endl;
 	return 0;
 }
