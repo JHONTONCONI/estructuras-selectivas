499c713eda5c854503509a684f24c3d5ea0b3ae8
JHONTONCONI <btaedwin123@gmail.com>
2025-06-01 15:47:29 -0400

    AGREGUE EL SEXTO EJERCICIO DE ESTRUCTURAS SELECTIVAS

---
+++ sexto_ejercicio.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	double promedio, pagopension, descuento, aumento, pagofinal;
+	cout<<nn *******************<<endl;
+	cout<< INGRESE EL PROMEDIO<<endl;
+	cout<< *******************<<endl;
+	cin>>promedio;
+
+	cout<<nn *******************************<<endl;
+	cout<< INGRESE EL PRECIO DE LA PENSION<<endl;
+	cout<< *******************************<<endl;
+	cin>>pagopension;
+	
+	if(promedio>=18){
+		descuento=pagopension*30/100;
+		pagofinal=pagopension-descuento;
+	}else{
+		aumento=pagopension*18/100*18/100;
+		pagofinal=pagopension+aumento;
+	}
+	cout<<EL PAGO A REALIZARSE ES: <<pagofinal<<endl;
+	return 0;
+}
