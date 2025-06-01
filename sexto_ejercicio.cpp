89bc3546967179f4d6bc5ed1726496f02ea20168
JHONTONCONI <btaedwin123@gmail.com>
2025-06-01 15:54:57 -0400

    MEJORE LA INTERFAZ

--- sexto_ejercicio.cpp
+++ sexto_ejercicio.cpp
@@ -4,23 +4,29 @@ using namespace std;
 
 int main(){
 	double promedio, pagopension, descuento, aumento, pagofinal;
-	cout<<nn *******************<<endl;
-	cout<< INGRESE EL PROMEDIO<<endl;
-	cout<< *******************<<endl;
+	cout<<"\n\n     *******************"<<endl;
+	cout<<"     INGRESE EL PROMEDIO"<<endl;
+	cout<<"     *******************"<<endl;
+	cout<<endl;
 	cin>>promedio;
-
-	cout<<nn *******************************<<endl;
-	cout<< INGRESE EL PRECIO DE LA PENSION<<endl;
-	cout<< *******************************<<endl;
+	cout<<"\n\n     *******************************"<<endl;
+	cout<<"     INGRESE EL PRECIO DE LA PENSION"<<endl;
+	cout<<"     *******************************"<<endl;
+	cout<<endl;
 	cin>>pagopension;
-	
-	if(promedio>=18){
-		descuento=pagopension*30/100;
-		pagofinal=pagopension-descuento;
+	if(promedio >= 18){
+		
+		descuento = pagopension*30/100;
+		pagofinal = pagopension-descuento;
+		
 	}else{
-		aumento=pagopension*18/100*18/100;
-		pagofinal=pagopension+aumento;
+		
+		aumento = pagopension*18/100*18/100;
+		pagofinal = pagopension+aumento;
+		
 	}
-	cout<<EL PAGO A REALIZARSE ES: <<pagofinal<<endl;
+	cout<<"\n\n     ***************************"<<endl;
+	cout<<"     EL PAGO A REALIZARSE ES: "<<pagofinal<<endl;
+	cout<<"     **************************"<<endl;
 	return 0;
 }
