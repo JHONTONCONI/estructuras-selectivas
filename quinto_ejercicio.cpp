dd3b73d95313f46184cb9cbfa5d3c9eddcec55e8
JHONTONCONI <btaedwin123@gmail.com>
2025-05-31 21:55:42 -0400

    MEJORE LA INTERFAZ DEL EJERCICIO

--- quinto_ejercicio.cpp
+++ quinto_ejercicio.cpp
@@ -4,26 +4,33 @@ using namespace std;
 
 int main(){
 	double pulsaciones, edad, genero;
-	cout<<nn *************************<<endl;
-	cout<< INGRESE SU EDAD POR FAVOR<<endl;
-	cout<< *************************<<endl;
+	cout<<"\n\n     *************************"<<endl;
+	cout<<"     INGRESE SU EDAD POR FAVOR"<<endl;
+	cout<<"     *************************"<<endl;
+	cout<<endl;
 	cin>>edad;
-	cout<<nn **********************************************<<endl;
-	cout<< COLOQUE 1 SI ES HOMBRE Y SI ES MUJER COLOQUE 0<<endl;
-	cout<< **********************************************<<endl;
+	cout<<endl;
+	cout<<"\n\n     **********************************************"<<endl;
+	cout<<"     COLOQUE 1 SI ES HOMBRE Y SI ES MUJER COLOQUE 0"<<endl;
+	cout<<"     **********************************************"<<endl;
+	cout<<endl;
 	cin>>genero;
+	cout<<endl;
 	if(genero==1){
 		pulsaciones=(210-edad)/10;
 	}else{
 		if(genero==0){
 			pulsaciones=(220-edad)/10;
 		}else{
-			cout<<no ingreso el valor correcto<<endl;
+			cout<<"\n\n     ***************************"<<endl;
+			cout<<"     no ingreso el valor correcto"<<endl;
+			cout<<"     ****************************"<<endl;
 			return 0;
 		}
 	}
-
-	cout<<EL NUMERO DE PULSACIONES QUE TIENE ES: <<pulsaciones<<endl;
+    cout<<"\n\n     *******************************************"<<endl;
+	cout<<"     EL NUMERO DE PULSACIONES QUE TIENE ES: "<<pulsaciones<<endl;
+	cout<<"     *******************************************"<<endl;
 	return 0;
 	
 	
